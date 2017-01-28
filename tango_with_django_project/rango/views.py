7879de460df05d85d86e56a00e627f8d6588669f
Rico <richardthomaswhitfield@gmail.com>
2017-01-28 12:50:40 -0600

    updated category view and template

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from rango.models import Category
+from rango.models import Page
 
 def index(request):
 	# Query the database for a list of ALL categories currently stored.
@@ -17,3 +18,31 @@ def index(request):
 def about(request):
 	about_dict = {'aboutmessage': "This tutorial has been put together by"}
 	return render(request, 'rango/about.html', context=about_dict)
+
+
+
+def show_category(request, category_name_slug):
+	context_dict = {}
+
+
+	try:
+		# Can we find a category name slug with the given name?
+		# If we can't, the .get() method raises a DoesNotExist exception.
+		# So the .get() method returns one model instance or raises an exception
+		category=Category.objects.get(slug=category_name_slug)
+
+		# Retrieve all of the associated pages.
+		# Note that filter() will return a list of page objects or an empty list
+		pages = Page.objects.filter(category=category)
+
+		# We also add the category object from
+		# the database to the context dictionary.
+		# We'll use this in the template to verify that the category exists
+
+		context_dict['pages'] = category
+
+	except Category.DoesNotExist:
+		context_dict['category'] = None
+		context_dict['pages'] = None
+
+	return render(request, 'rango/category.html', context_dict)
