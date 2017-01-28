bd8e66173b5c7b6ff15b4ce1a4a07de980eef9b0
Rico <richardthomaswhitfield@gmail.com>
2017-01-27 21:47:27 -0600

    updated media dir

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -5,4 +5,5 @@ def index(request):
 	context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
 	return render(request, 'rango/index.html', context=context_dict)
 def about(request):
-	return HttpResponse("Whats this all about, Rango?<br/><a href='/rango/'>Rango</a>")
+	about_dict = {'aboutmessage': "This is a tutorial I am doing"}
+	return render(request, 'rango/about.html', context=about_dict)
